f032c9c4299550f9bde8912fa8a162a217272346
SJMdev <sjorsmallon@gmail.com>
2017-12-01 12:03:37 +0100

    fixed

--- www/index.php
+++ www/index.php
@@ -3,52 +3,156 @@
 include '../util.php';
 include '../solver.php';
 include '../reader.php';
+include '../formatter.php';
 
-date_default_timezone_set('Europe/Amsterdam');
-
-$errors = array();
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST'
-	&& isset($_FILES['knowledgebase'])
-	&& $file = process_file($_FILES['knowledgebase'], $errors))
+function _encode($data)
 {
-	switch ($_POST['action'])
-	{
-		case 'analyse':
-			header('Location: analyse.php?kb=' . rawurlencode($file));
-			break;
-
-		case 'run':
-			header('Location: webfrontend.php?kb=' . rawurlencode($file));
-			break;
-	}
-
-	exit;
+	return base64_encode(gzcompress(serialize($data)));
 }
 
-function process_file($file, array &$errors = array())
+function _decode($data)
 {
-	if ($file['error'] != 0)
-		return "Er is een fout opgetreden bij het uploaden.";
-
-	$reader = new KnowledgeBaseReader;
-	$errors = $reader->lint($file['tmp_name']);
-
-	$unique_name = sha1(microtime() . uniqid('kb', true)) . '.xml';
-
-	if (count($errors) > 0)
-		return false;
-
-	if (!move_uploaded_file($file['tmp_name'], '../knowledgebases/' . $unique_name))
-	{
-		$errors[] = "De knowledge-base kon niet worden opgeslagen op de server.";
-		return false;
-	}
-	
-	return $unique_name;
+	return unserialize(gzuncompress(base64_decode($data)));
 }
 
-$template = new Template('templates/single.phtml');
-$template->errors = $errors;
+class WebLogger implements Logger
+{
+	public $messages = array(array());
 
-echo $template->render();
+	public function __wakeup()
+	{
+		$this->messages[] = array();
+	}
+
+	public function write($format, $arguments, $level)
+	{
+		$arguments = array_map(function($arg) {
+			return '<tt>' . Template::html(to_debug_string($arg)) . '</tt>';
+		}, $arguments);
+
+		$this->messages[count($this->messages) - 1][] = [$level, vsprintf($format, $arguments)];
+	}
+}
+
+class WebFrontend
+{
+	private $log;
+
+	private $solver;
+
+	private $state;
+
+	private $kb_file;
+
+	public function __construct($kb_file)
+	{
+		$this->kb_file = $kb_file;
+	}
+
+	public function main()
+	{
+		$this->log = $this->getLog();
+
+		$this->solver = new Solver($this->log);
+
+		try
+		{
+			$this->state = $this->getState();
+
+			if (isset($_POST['answer']))
+				$this->state->apply(_decode($_POST['answer']));
+
+			$step = $this->solver->solveAll($this->state);
+
+			$page = new Template('templates/layout.phtml');
+
+			if ($step instanceof AskedQuestion)
+				$page->content = $this->displayQuestion($step);
+			else
+				$page->content = $this->displayConclusions();
+		}
+		catch (Exception $e)
+		{
+			$page = new Template('templates/exception.phtml');
+			$page->exception = $e;
+		}
+
+		$page->state = $this->state;
+
+		$page->log = $this->log;
+
+		echo $page->render();
+	}
+
+	private function displayQuestion(AskedQuestion $question)
+	{
+		$template = new Template('templates/question.phtml');
+
+		$template->state = $this->state;
+
+		$template->question = $question;
+
+		return $template->render();
+	}
+
+	private function displayConclusions()
+	{
+		$template = new Template('templates/completed.phtml');
+
+		$template->state = $this->state;
+
+		return $template->render();
+	}
+
+	private function getState()
+	{
+		if (isset($_POST['state']))
+			return _decode($_POST['state']);
+		else
+			return $this->createNewState();
+	}
+
+	private function createNewState()
+	{
+		$state = $this->readState($this->kb_file);
+
+		if (!empty($_GET['goals']))
+			foreach (explode(',', $_GET['goals']) as $goal)
+				$state->goalStack->push($goal);
+		else
+			foreach ($state->goals as $goal)
+			{
+				$state->goalStack->push($goal->name);
+
+				// Also push any answer values that are variables as goals to be solved.
+				foreach ($goal->answers as $answer)
+					if (KnowledgeState::is_variable($answer->value))
+						$state->goalStack->push(KnowledgeState::variable_name($answer->value));	
+			}
+
+		return $state;
+	}
+
+	private function readState($file)
+	{
+		$reader = new KnowledgeBaseReader;
+		$state = $reader->parse($file);
+		
+		return $state;
+	}
+
+	private function getLog()
+	{
+		if (isset($_POST['log']))
+			return _decode($_POST['log']);
+		else
+			return new WebLogger();
+	}
+}
+
+if (!isset($_GET['kb']) || !preg_match('/^[a-zA-Z0-9_\-\.]+\.xml$/i', $_GET['kb']))
+	redirect('index.php');
+
+header('Content-Type: text/html; charset=UTF-8');
+$frontend = new WebFrontend('../knowledgebases/knowledge.xml');
+$frontend->main();
